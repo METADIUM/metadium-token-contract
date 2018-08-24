a3108d12ce798cd7e7f3ce54c838c130f1381261
gijinBang <rlwlsdnlwjem2@gmail.com>
2018-08-24 13:50:26 +0900

    modified readme

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,14 +1,10 @@
 const Metadium = artifacts.require('Metadium.sol')
 const MetadiumVesting = artifacts.require('MetadiumVesting.sol')
-// truffle migrate metadium --reset
 module.exports = deployer => {
   const args = process.argv.slice()
   if (args[3] == 'metadium') {
     deployer.deploy(Metadium);
   } else if (args[3] == 'metadiumVesting') {
-    //console.log(args[4].split(","))
-    // truffle migrate metadiumVesting 0x6f0f5673d69b4608ac9be5887b2f71f20d0c3587 1523867000 300 300 "true" --reset
-    // truffle migrate metadiumVesting 0x6f0f5673d69b4608ac9be5887b2f71f20d0c3587 1523867000 300 300 '' --reset  : false 로 할 때
     deployer.deploy(MetadiumVesting, args[4],args[5],args[6],args[7],args[8]);
   }
 }
